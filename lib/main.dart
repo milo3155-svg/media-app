dd1b568a8edf1412dc3c010a92b1475e0e256571
milo3155-svg <milo3155@gmail.com>
2026-08-09 08:32:54 -0600

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package0package:flutter/material.dart';
+
+void main() {
+  runApp(const MediaApp());
+}
+
+class MediaApp extends StatelessWidget {
+  const MediaApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Aplicación Multimedia',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: const Color(0xFF121212),
+        primaryColor: Colors.deepPurple,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Aplicación Multimedia'),
+        backgroundColor: Colors.black,
+      ),
+      body: const Center(
+        child: Text(
+          '¡Bienvenido a tu App!',
+          style: TextStyle(fontSize: 20, color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
