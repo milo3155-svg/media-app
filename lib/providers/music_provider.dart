e78fc71317bc7df377cd3bf1e55a51af4c9f6c38
milo3155-svg <milo3155@gmail.com>
2026-08-25 16:51:44 -0600

    Update music_provider.dart

--- lib/providers/music_provider.dart
+++ lib/providers/music_provider.dart
@@ -29,19 +29,15 @@ class MusicProvider extends ChangeNotifier {
       String? audioUrl = await ApiService.getAudioUrl(videoId);
       
       if (audioUrl != null) {
-        // En lugar de setUrl directo, usamos setAudioSource con el disfraz de Chrome
         await _audioPlayer.setAudioSource(
           AudioSource.uri(
             Uri.parse(audioUrl),
-            headers: {
-              'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
-              'Referer': 'https://www.youtube.com/',
-            },
+            headers: { 'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36' },
           ),
         );
         _audioPlayer.play();
       } else {
-        _currentTrack = 'Error: No se pudo obtener el audio';
+        _currentTrack = 'Error: Sin URL de audio';
       }
     } catch (e) {
       _currentTrack = 'Fallo: $e';
