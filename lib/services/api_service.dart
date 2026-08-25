ad577918db61e3c4c822de5f7b8aab6ea4bfbbfe
milo3155-svg <milo3155@gmail.com>
2026-08-25 10:28:30 -0600

    Update api_service.dart

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,8 +1,11 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:youtube_explode_dart/youtube_explode_dart.dart';
 
 class ApiService {
   static final _yt = YoutubeExplode();
 
+  // 1. Mantenemos YoutubeExplode para las búsquedas (es el más rápido)
   static Future<List<dynamic>> search(String query) async {
     if (query.trim().isEmpty) return [];
     
@@ -23,12 +26,26 @@ class ApiService {
     }
   }
 
-  // ¡Atención aquí! Quitamos el try-catch. 
-  // Si YouTube rechaza la conexión, el error explotará y viajará
-  // directo a tu MusicProvider para pintarse en la pantalla.
+  // 2. NUEVA ESTRATEGIA: Usamos una API externa (Piped) solo para obtener el enlace limpio del audio
   static Future<String?> getAudioUrl(String videoId) async {
-    var manifest = await _yt.videos.streamsClient.getManifest(videoId);
-    var streamInfo = manifest.audioOnly.withHighestBitrate();
-    return streamInfo.url.toString();
+    try {
+      // Pedimos los enlaces a un servidor proxy público que burla el bloqueo 403
+      final url = Uri.parse('https://pipedapi.kavin.rocks/streams/$videoId');
+      final response = await http.get(url);
+      
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        final audioStreams = data['audioStreams'] as List;
+        
+        if (audioStreams.isNotEmpty) {
+          // Tomamos el primer stream de audio disponible (suele ser formato m4a nativo para Android)
+          return audioStreams[0]['url'];
+        }
+      }
+      return null;
+    } catch (e) {
+      print("Error en extracción híbrida: $e");
+      return null;
+    }
   }
 }
