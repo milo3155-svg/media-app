b385cf7126f9b033e244a937b800f931f79940fb
milo3155-svg <milo3155@gmail.com>
2026-08-28 15:32:53 -0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:just_audio_background/just_audio_background.dart';
 import 'providers/music_provider.dart';
 import 'screens/home_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  // Inicialización de segundo plano requerida por just_audio_background
-  try {
-    await JustAudioBackground.init(
-      androidNotificationChannelId: 'com.milo.media_app.channel.audio',
-      androidNotificationChannelName: 'Reproducción de Música',
-      androidNotificationOngoing: true,
-    );
-  } catch (e) {
-    debugPrint('Error inicializando notificaciones: $e');
-  }
-
   runApp(const MediaApp());
 }
 
@@ -35,8 +22,7 @@ class MediaApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         theme: ThemeData.dark().copyWith(
           scaffoldBackgroundColor: const Color(0xFF121212),
-          primaryColor: Colors.purpleAccent,
-          colorScheme: const ColorScheme.dark(
+          colorScheme: ColorScheme.dark(
             primary: Colors.purpleAccent,
             secondary: Colors.purpleAccent,
           ),
